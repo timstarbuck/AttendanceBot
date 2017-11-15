4825eeb35db44e83b668d916a9e9613f6f24d339
Tim Starbuck <timstarbuck@gmail.com>
2017-11-15 13:20:33 -0600

    Add getByPerson

--- AttendanceBot/Models/LocationRecords.cs
+++ AttendanceBot/Models/LocationRecords.cs
@@ -24,5 +24,23 @@ namespace AttendanceBot
         }
 
         public static Dictionary<string, List<LocationRecord>> Logs { get; set; }
+
+        public static List<LocationRecord> GetByPerson(string personName, string groupName)
+        {
+            var records = new List<LocationRecord>();
+            var face = new FaceServices();
+            var people = new FaceServices().ListPeople(groupName).Result;
+
+            var person = people.FirstOrDefault((p) => p.Name.Equals(personName, StringComparison.InvariantCultureIgnoreCase));
+            if (person != null)
+            {
+                if (LocationRecordLogs.Logs.ContainsKey(person.PersonId))
+                {
+                    records = LocationRecordLogs.Logs[person.PersonId];
+                }
+            }
+
+            return records;
+        }
     }
 }
