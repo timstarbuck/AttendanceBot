85a849ff136b595c040da80a77bf4ca944a396e0
zxhorompoli <zxhorompoli@west.com>
2017-11-15 10:47:55 -0600

    Added face lookup

---
+++ AttendanceBot/Controllers/StudentSearchController.cs
@@ -0,0 +1,156 @@
+﻿using AttendanceBot.Services;
+using Microsoft.Bot.Connector;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace AttendanceBot.Controllers
+{
+    [BotAuthentication]
+    public class StudentSearchController : ApiController
+    {
+        const string personGroupId = "test";
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            if (activity.Type == ActivityTypes.Message)
+            {
+                //await Conversation.SendAsync(activity, () => new Dialogs.RootDialog());
+                var connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+                string message = "";
+
+                try
+                {
+                    message = await this.GetImageIdentityAsync(activity, connector);
+                }
+                catch (ArgumentException e)
+                {
+                    message = "Did you upload an image? I'm more of a visual person. " +
+                        "Try sending me an image";
+
+                    Trace.TraceError(e.ToString());
+                }
+                catch (Exception e)
+                {
+                    message = "Oops! Something went wrong. Try again later";
+                    //if (e is ClientException && (e as ClientException).Error.Message.ToLowerInvariant().Contains("access denied"))
+                    //{
+                    //    message += " (access denied - hint: check your APIKEY at web.config).";
+                    //}
+
+                    Trace.TraceError(e.ToString());
+                }
+
+                Activity reply = activity.CreateReply(message);
+                await connector.Conversations.ReplyToActivityAsync(reply);
+            }
+            else
+            {
+                HandleSystemMessage(activity);
+            }
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+        private Activity HandleSystemMessage(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                if (message.MembersAdded[0].Name != "Bot")
+                {
+                    ConnectorClient client = new ConnectorClient(new Uri(message.ServiceUrl));
+                    var reply = message.CreateReply();
+                    reply.Text = "Hello. How may I help you?";
+                    client.Conversations.ReplyToActivityAsync(reply);
+                }
+
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                //var message = await result;
+                //await context.PostAsync("Hello. How may I help you?");
+                //context.Wait(MessageReceivedAsync);
+
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+            }
+
+            return null;
+        }
+
+        private async Task<string> GetImageIdentityAsync(Activity activity, ConnectorClient connector)
+        {
+            var face = new RecognitionServices();
+
+            var imageAttachment = activity.Attachments?.FirstOrDefault(a => a.ContentType.Contains("image"));
+            if (imageAttachment != null)
+            {
+                using (var stream = await GetImageStream(connector, imageAttachment))
+                {
+                    return await face.FindPersonFromImage(stream, personGroupId);
+                }
+            }
+
+            // If we reach here then the activity is neither an image attachment nor an image URL.
+            throw new ArgumentException("The activity doesn't contain a valid image attachment.");
+        }
+
+        private static async Task<Stream> GetImageStream(ConnectorClient connector, Attachment imageAttachment)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                // The Skype attachment URLs are secured by JwtToken,
+                // you should set the JwtToken of your bot as the authorization header for the GET request your bot initiates to fetch the image.
+                // https://github.com/Microsoft/BotBuilder/issues/662
+                var uri = new Uri(imageAttachment.ContentUrl);
+                if (uri.Host.EndsWith("skype.com") && uri.Scheme == "https")
+                {
+                    httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", await GetTokenAsync(connector));
+                    httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/octet-stream"));
+                }
+
+                return await httpClient.GetStreamAsync(uri);
+            }
+        }
+
+        /// <summary>
+        /// Gets the JwT token of the bot. 
+        /// </summary>
+        /// <param name="connector"></param>
+        /// <returns>JwT token of the bot</returns>
+        private static async Task<string> GetTokenAsync(ConnectorClient connector)
+        {
+            var credentials = connector.Credentials as MicrosoftAppCredentials;
+            if (credentials != null)
+            {
+                return await credentials.GetTokenAsync();
+            }
+
+            return null;
+        }
+
+    }
+}
---
+++ AttendanceBot/Models/FaceDetectionRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AttendanceBot.Models
+{
+    public class FaceDetectionRequest
+    {
+        public string personGroupId { get; set; }
+        public List<string> faceIds { get; set; }
+        public int maxNumOfCandidatesReturned { get; set; }
+        public double confidenceThreshold { get; set; }
+    }
+}
